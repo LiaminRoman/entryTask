b195c32d35615aae59d05781e8dd2341878e4a50
Roman Liamin <LiaminRoman@gmail.com>
2016-10-20 19:10:07 +0300

    Task DONE

---
+++ entryTest/LargestFinder.java
@@ -0,0 +1,73 @@
+package entryTest;
+
+/**
+ * Created by lr on 20.10.2016.
+ */
+public class LargestFinder {
+    public int[] getLargest(int[] array, int k) {
+        int elementsToFound = k;
+        if (wrongArguments(array, elementsToFound))
+            return new int[0];
+
+        if (array.length <= elementsToFound) {
+            int largestElements[] = new int[array.length];
+            System.arraycopy(array, 0, largestElements, 0, array.length);
+
+            return largestElements;
+        }
+
+        int largestElements[] = new int[elementsToFound];
+
+        for (int i=0; i<array.length; i++) {
+            int foundElements = i < elementsToFound ? i : elementsToFound;
+
+            int elementToCheck = array[i];
+            int index = binarySearch(largestElements, 0, foundElements, elementToCheck);
+            int positionToPaste = index >= 0 ? index : -index - 1;
+            if (positionToPaste >= elementsToFound) {
+                continue;
+            }
+
+            if (positionToPaste < foundElements) {
+                int elementsToMove = foundElements - positionToPaste;
+                if (positionToPaste + elementsToMove == elementsToFound) {
+                    elementsToMove--;
+                }
+
+                if (elementsToMove > 0)
+                    System.arraycopy(largestElements, positionToPaste, largestElements, positionToPaste + 1, elementsToMove);
+            }
+
+            largestElements[positionToPaste] = elementToCheck;
+        }
+        return largestElements;
+    }
+
+    private boolean wrongArguments(int[] array, int k) {
+        if (array == null || array.length == 0) {
+            return true;
+        }
+        if (k <= 0) {
+            return true;
+        }
+        return false;
+    }
+
+    private int binarySearch(int[] a, int fromIndex, int toIndex, int key) {
+        int low = fromIndex;
+        int high = toIndex - 1;
+
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int midVal = a[mid];
+
+            if (midVal < key)
+                high = mid - 1;
+            else if (midVal > key)
+                low = mid + 1;
+            else
+                return mid; // key found
+        }
+        return -(low + 1);  // key not found.
+    }
+}
---
+++ entryTest/LargestFinderTest.java
@@ -0,0 +1,175 @@
+package entryTest;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by lr on 20.10.2016.
+ */
+public class LargestFinderTest {
+    LargestFinder largestFinder = new LargestFinder();
+
+    @Test
+    public void testNullArray() {
+        int[] array = null;
+        int countToFind = 1;
+
+        int[] largestElements = largestFinder.getLargest(array, countToFind);
+
+        assertThat(largestElements, is(not(nullValue())));
+        assertThat(largestElements.length, is(0));
+    }
+
+    @Test
+    public void testEmptyArray() {
+        int[] array = new int[0];
+        int countToFind = 1;
+
+        int[] largestElements = largestFinder.getLargest(array, countToFind);
+
+        assertThat(largestElements, is(not(nullValue())));
+        assertThat(largestElements.length, is(0));
+    }
+
+    @Test
+    public void testInvalidCountElementsToFind() {
+        int[] array = {1, 2, 3, 4, 5};
+        int countToFind = -1;
+
+        int[] largestElements = largestFinder.getLargest(array, countToFind);
+
+        assertThat(largestElements, is(not(nullValue())));
+        assertThat(largestElements.length, is(0));
+    }
+
+    @Test
+    public void testCountElementsToFindIncreaseArraySize() {
+        int[] array = {1, 2, 3, 4, 5};
+        int countToFind = array.length+2;
+
+        int[] largestElements = largestFinder.getLargest(array, countToFind);
+
+        assertThat(largestElements, is(not(nullValue())));
+        assertThat(largestElements, equalTo(array));
+    }
+
+    @Test
+    public void testArray1() {
+        int[] array = {10, 9, 8, 8, 6};
+        int countToFind = 2;
+
+        int[] largestElements = largestFinder.getLargest(array, countToFind);
+
+        assertThat(largestElements, is(not(nullValue())));
+        assertThat(largestElements, equalTo(new int[] {10, 9}));
+    }
+
+    @Test
+    public void testWarySmallArray() {
+        int[] array = {2, 10};
+        int countToFind = 1;
+
+        int[] largestElements = largestFinder.getLargest(array, countToFind);
+
+        assertThat(largestElements, is(not(nullValue())));
+        assertThat(largestElements, equalTo(new int[] {10}));
+    }
+
+    @Test
+    public void testSmallArray() {
+        int[] array = {2, 4, 6, 8, 10};
+        int countToFind = 2;
+
+        int[] largestElements = largestFinder.getLargest(array, countToFind);
+
+        assertThat(largestElements, is(not(nullValue())));
+        assertThat(largestElements, equalTo(new int[] {10, 8}));
+    }
+
+    @Test
+    public void testLookingForAlmostAllArray() {
+        int[] array = {10, 9, 8, 8, 6};
+        int countToFind = 4;
+
+        int[] largestElements = largestFinder.getLargest(array, countToFind);
+
+        assertThat(largestElements, is(not(nullValue())));
+        assertThat(largestElements, equalTo(new int[] {10, 9, 8, 8}));
+    }
+
+    @Test
+    public void testLookingForAlmostAllArrayButArrayElementsAreEqual() {
+        int[] array = {10, 10, 10, 10, 10};
+        int countToFind = 4;
+
+        int[] largestElements = largestFinder.getLargest(array, countToFind);
+
+        assertThat(largestElements, is(not(nullValue())));
+        assertThat(largestElements, equalTo(new int[] {10, 10, 10, 10}));
+    }
+
+    @Test
+    public void testLookingForOneElementButArrayElementsAreEqual() {
+        int[] array = {10, 10, 10, 10, 10};
+        int countToFind = 1;
+
+        int[] largestElements = largestFinder.getLargest(array, countToFind);
+
+        assertThat(largestElements, is(not(nullValue())));
+        assertThat(largestElements, equalTo(new int[] {10}));
+    }
+
+    @Test
+    public void testLookingForOneElementItLastInArray() {
+        int[] array = {0, 0, 0, 0, 10};
+        int countToFind = 1;
+
+        int[] largestElements = largestFinder.getLargest(array, countToFind);
+
+        assertThat(largestElements, is(not(nullValue())));
+        assertThat(largestElements, equalTo(new int[] {10}));
+    }
+
+    @Test
+    public void testLookingForOneElementItFirstInArray() {
+        int[] array = {10, 0, 0, 0, 0};
+        int countToFind = 1;
+
+        int[] largestElements = largestFinder.getLargest(array, countToFind);
+
+        assertThat(largestElements, is(not(nullValue())));
+        assertThat(largestElements, equalTo(new int[] {10}));
+    }
+
+    @Test
+    public void testLargeArray() {
+        int arraySize = 10_000_000;
+        int[] array = new int[arraySize];
+        Random random = new Random();
+        int maxValue = 10_000;
+        int countToFind = 10_000;
+
+        for (int i=0; i<arraySize; i++) {
+            array[i] = random.nextInt(maxValue);
+        }
+
+        int needResult[] = new int[countToFind];
+
+        int position = 74;
+        for (int i = 0; i < countToFind; i++) {
+            final int currentValue = maxValue * 10 + i;
+            needResult[countToFind - i - 1] = currentValue;
+            array[position] = currentValue;
+            position += 33;
+        }
+
+        int[] largestElements = largestFinder.getLargest(array, countToFind);
+
+        assertThat(largestElements, is(not(nullValue())));
+        assertThat(largestElements, equalTo(needResult));
+    }
+}
